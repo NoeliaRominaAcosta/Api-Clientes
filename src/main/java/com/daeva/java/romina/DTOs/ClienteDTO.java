ab54bf18b4a12fd0b040afd3dcce6a65c29374f5
NoeliaRominaAcosta <noeliaromina20@gmail.com>
2024-11-06 20:00:52 -0300

    Fixed mapper cliente

--- src/main/java/com/daeva/java/romina/DTOs/ClienteDTO.java
+++ src/main/java/com/daeva/java/romina/DTOs/ClienteDTO.java
@@ -4,13 +4,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class ClienteDTO {
     private Long id;
     private String nombre;
+    private String apellido;
     private String email;
+    private String dni;
+    private String telefono;
+    private String direccion;
+    private String ciudad;
+    private String codigoPostal;
+    private LocalDate fechaNacimiento;
+    private String genero;
+    private LocalDate fechaRegistro;
 
 }
 
--- src/main/java/com/daeva/java/romina/Mappers/ClienteMapper.java
+++ src/main/java/com/daeva/java/romina/Mappers/ClienteMapper.java
@@ -9,17 +9,33 @@ public class ClienteMapper {
         ClienteDTO dto = new ClienteDTO();
         dto.setId(cliente.getId());
         dto.setNombre(cliente.getNombre());
+        dto.setApellido(cliente.getApellido());
         dto.setEmail(cliente.getEmail());
-        // asignar otros campos necesarios
+        dto.setDireccion(cliente.getDni());
+        dto.setCiudad(cliente.getCiudad());
+        dto.setGenero(cliente.getCiudad());
+        dto.setFechaNacimiento(cliente.getFechaNacimiento());
+        dto.setFechaRegistro(cliente.getFechaRegistro());
+        dto.setCodigoPostal(cliente.getCodigoPostal());
+        dto.setDni(cliente.getDni());
+        dto.setTelefono(cliente.getTelefono());
         return dto;
     }
 
     public static Cliente toEntity(ClienteDTO clienteDTO) {
         Cliente cliente = new Cliente();
         cliente.setId(clienteDTO.getId());
+        cliente.setApellido(clienteDTO.getApellido());
         cliente.setNombre(clienteDTO.getNombre());
         cliente.setEmail(clienteDTO.getEmail());
-        // asignar otros campos necesarios
+        cliente.setDireccion(clienteDTO.getDireccion());
+        cliente.setCiudad(clienteDTO.getCiudad());
+        cliente.setGenero(clienteDTO.getGenero());
+        cliente.setFechaNacimiento(clienteDTO.getFechaNacimiento());
+        cliente.setFechaRegistro(clienteDTO.getFechaRegistro());
+        cliente.setCodigoPostal(clienteDTO.getCodigoPostal());
+        cliente.setDni(clienteDTO.getDni());
+        cliente.setTelefono(clienteDTO.getTelefono());
         return cliente;
     }
 }
