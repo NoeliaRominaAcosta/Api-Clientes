57e9252d4aa39c6c6e617bb2f5b946033818cc77
NoeliaRominaAcosta <noeliaromina20@gmail.com>
2024-11-06 21:30:25 -0300

    OpenAPI swagger implemented!

---
+++ src/main/java/com/daeva/java/romina/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.daeva.java.romina.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Mi API Rest")
+                        .version("v1")
+                        .description("Documentación de mi API Rest")
+                );
+    }
+}
